c4b933e105d9f7ea307ec482527494a25de23ffd
ayef <ayesha.ahmad@helsinki.fi>
2014-04-19 12:12:46 +0300

    Readings, and statistics screens added. They are empty

--- src/com/sensorcon/ReadingsActivity.java
+++ src/com/sensorcon/ReadingsActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +14,7 @@ import com.sensorcon.sensordrone.DroneEventHandler;
 import com.sensorcon.sensordrone.DroneEventObject;
 import com.sensorcon.sensordrone.android.Drone;
 
-public class MainActivity extends Activity {
+public class ReadingsActivity extends Activity {
 
     // UI Elements
     Button btnConnect;
@@ -27,61 +26,38 @@ public class MainActivity extends Activity {
     // Sensordrone Objects
     Drone myDrone;
     DroneEventHandler myDroneEventHandler;
- //   DroneConnectionHelper myHelper;
     
-    /** Called when the user clicks the Settings button
-     * Opens the Settings Activity */
-    public void viewSettings(View view) {
-    	Intent intent = new Intent(this, SettingsActivity.class);
-    	startActivity(intent);
-    	
-    }
-
-    /** Called when the user clicks the 'View Readings' button 
-     * Opens the Readings Activity */
-
-    public void viewReadings(View view) {
-    	Intent intent = new Intent(this, ReadingsActivity.class);
-    	startActivity(intent);
-    	
-    }
-
-    /** Called when the user clicks the 'View Statistics' button  
-     * Opens the Statistics Activity */
-    public void viewStatistics(View view) {
-    	Intent intent = new Intent(this, StatisticsActivity.class);
-    	startActivity(intent);
-    	
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        // Set up our Sensordrone object
+        setContentView(R.layout.readings);
         myDrone = new Drone();
-
         myDrone.btConnect("00:17:EC:11:C0:0F");
-        
-    
-
-        // We have set up a simple layout named main.xml
-        // There are three Buttons, and two TextViews that we will interact with
-
-        // This TextView will be used to show our connection status (connected/not connected/connection lost)
         tvStatus = (TextView)findViewById(R.id.main_tv_connection_status);
-
-        // Most all of these events are triggered from a background worker thread.
-        // you need to remember this if you plan to update UI elements!
-        // The method below shows how to easily do this.
+        tvTemperature = (TextView)findViewById(R.id.main_tv_temperature);
         updateTextViewFromUI(tvStatus, "Connected");
+        myDrone.enableTemperature();
+        myDrone.enableHumidity();
 
-        // Since we know we want to measure temperature, we will automatically enable the
-        // temperature sensor here at every connect. Now the user won't have to worry about it.
-        //myDrone.enableTemperature();
-        
-     
+     // This button will take a Temperature measurement when pressed
+        btnMeasure = (Button)findViewById(R.id.main_btn_refresh);
+        btnMeasure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (myDrone.isConnected) {
+                	myDrone.measureHumidity();
+                    myDrone.measureTemperature();
+                }
+                else if (myDrone.isConnected && !myDrone.temperatureStatus) {
+                    // If the sensor isn't enabled, tell the user
+                    genericDialog("Whoa!","The temperature sensor hasn't been enabled!");
+                }
+                else {
+                    // If we weren't connected, tell the user
+                    genericDialog("Whoa!","You are not currently connected to a Sensordrone");
+                }
+            }
+        });
         // The events triggered are run through a DroneEventHandler.
         // This is where the magic happens :-)
         // DroneEventHandler is an interface, so things like MainActivity or even a custom
@@ -115,6 +91,7 @@ public class MainActivity extends Activity {
                     // Since we know we want to measure temperature, we will automatically enable the
                     // temperature sensor here at every connect. Now the user won't have to worry about it.
                     myDrone.enableTemperature();
+                    myDrone.enableHumidity();
                 }
                 else if (droneEventObject.matches(DroneEventObject.droneEventType.DISCONNECTED)) {
                     // This is triggered when the disconnect method is called.
@@ -135,14 +112,9 @@ public class MainActivity extends Activity {
                 }
                 else if (droneEventObject.matches(DroneEventObject.droneEventType.TEMPERATURE_ENABLED)) {
                     // This is triggered when the temperature has been enabled
+                	myDrone.measureHumidity();
+                	myDrone.measureTemperature();
 
-                    // Let's go ahead and take a reading for the user!
-                    // Since we have set it up so connecting to the Sensordrone triggers enabling
-                    // the temperature, and enabling temperature triggers measuring temperature,
-                    // the user will always be displayed a temp reading when first connecting via this app.
-                    // You don't have to keep this type of configuration, but you can 'daisy chain' commands
-                    // in this manner, which can be useful in certain situations.
-                    myDrone.measureTemperature();
                 }
                 else if (droneEventObject.matches(DroneEventObject.droneEventType.TEMPERATURE_MEASURED)) {
                     // This gets fired when the temperature is measured
@@ -151,16 +123,15 @@ public class MainActivity extends Activity {
                     // You can put in logic to control what units are displayed,
                     // but we will just show Celsius here.
                     //
-                    // Note that the Sensordrone library provides some values in different units
-                    // (for temperature it is Celsius, Fahrenheit, and Kelvin). You can obviously
-                    // take one of those numbers and put it to any unit you want. If there is some
-                    // unit you want in the library, let us know,
-                    // or fork them code, add it, and send in a pull request on github!
                     String temp = String.format("%.2f \u00B0C",myDrone.temperature_Celsius);
                     updateTextViewFromUI(tvTemperature, temp);
-
+                    
+                    
+                    String temp1 = String.format("%.2f",myDrone.humidity_Percent);
+                    temp1 = temp1 + " %";
+                    updateTextViewFromUI((TextView)findViewById(R.id.main_tv_humidity), temp1);
                     // We will show a toast message to assure the user a measurement has been made.
-                    uiToast("Temperature updated!");
+                    uiToast("Values Refreshed!");
 
                 }
                 else if (droneEventObject.matches(DroneEventObject.droneEventType.TEMPERATURE_DISABLED)) {
@@ -200,7 +171,7 @@ public class MainActivity extends Activity {
     // A method to show a generic alert dialog with the supplied title and content
     public void genericDialog(String title, String msg) {
         Dialog dialog;
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(ReadingsActivity.this);
         builder.setTitle(title);
         builder.setMessage(msg);
         builder.setIcon(R.drawable.ic_launcher);
@@ -216,7 +187,7 @@ public class MainActivity extends Activity {
 
     // A method to update a TextView from the UI thread
     public void updateTextViewFromUI(final TextView textView, final String text) {
-        MainActivity.this.runOnUiThread(new Runnable() {
+        ReadingsActivity.this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 textView.setText(text);
@@ -226,10 +197,10 @@ public class MainActivity extends Activity {
 
     // A method to display a Toast notification from the UI thread
     public void uiToast(final String msg) {
-        MainActivity.this.runOnUiThread(new Runnable() {
+        ReadingsActivity.this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                Toast.makeText(ReadingsActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
     }
